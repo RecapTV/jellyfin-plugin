6a8b076afd97ec0a5a64da87d03a79c551a215ec
Nuno Morais <nunodpmorais@gmail.com>
2026-08-13 16:14:23 +0100

    fix: update icon handling in menu entry to use font icons

--- Jellyfin.Plugin.RecapTV/Web/RecapTV-client.js
+++ Jellyfin.Plugin.RecapTV/Web/RecapTV-client.js
@@ -158,10 +158,15 @@
     var label = findLabelNode(entry);
     if (label) label.textContent = "RecapTV";
 
-    var icon = entry.querySelector("svg");
-    if (icon) {
-      icon.innerHTML =
-        '<path d="M21 3H3c-1.11 0-2 .9-2 2v12c0 1.1.89 2 2 2h5v2h8v-2h5c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"></path>';
+    var fontIcon = entry.querySelector(".material-icons");
+    if (fontIcon) {
+      fontIcon.classList.remove.apply(
+        fontIcon.classList,
+        Array.prototype.filter.call(fontIcon.classList, function (c) {
+          return c !== "material-icons" && c.indexOf("listItemIcon") !== 0;
+        })
+      );
+      fontIcon.classList.add("live_tv");
     }
 
     entry.addEventListener("click", function (e) {
